6c2777194d513bff945b748a2f8839c4c30e3cdd
kLot2005 <bekars.bihimbai2005@gmail.com>
2025-01-31 20:16:45 +0500

    done

--- app/src/models/User.java
+++ app/src/models/User.java
@@ -1,7 +1,7 @@
 package models;
 
 public class User {
-
+    private int id;
     private String username;
     private String password;
     private String role;
@@ -12,6 +12,13 @@ public class User {
     }
 
     public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(int id, String username, String password, String role) {
+        this.id = id;
         this.username = username;
         this.password = password;
         this.role = role;
@@ -28,11 +35,39 @@ public class User {
     public String getRole() {
         return role;
     }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
     public boolean isAdmin() {
         return role.equalsIgnoreCase("Admin");
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
 
 
