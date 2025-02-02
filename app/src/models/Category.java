cb90653e9650b51f8dfcc2f549d4637d2588072a
kLot2005 <bekars.bihimbai2005@gmail.com>
2025-02-02 23:46:56 +0500

    исправил

--- app/src/models/Category.java
+++ app/src/models/Category.java
@@ -1,9 +1,14 @@
+package models;
+
 public class Category {
     private int id;
     private String name;
 
     public Category(int id, String name) {
         this.id = id;
+        this.name = name;
+    }
+    public Category(String name) {
         this.name = name;
     }
 
